01098bea2e187b5e5af7a08699f07e3793e1b029
Senn Dominik <Senn Dominik>
2016-05-07 09:50:51 +0200

    Completed 1st version of Dealers Den Data importer

--- Server/Imports/ImportDealersDenPackage.php
+++ Server/Imports/ImportDealersDenPackage.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 define('ROOT_PATH', getcwd() . '/');
 define('SHARED_PATH', ROOT_PATH . '../Shared/');
@@ -12,7 +12,11 @@ require VENDOR_PATH . 'parsecsv/php-parsecsv/parsecsv.lib.php';
 
 use \YaLinqo\Enumerable;
 
-$db = new MeekroDB(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT, DB_CHARSET);
+set_error_handler("exceptionErrorHandler", E_ALL);
+
+$database = new MeekroDB(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT, DB_CHARSET);
+$database->throw_exception_on_error = true;
+$database->error_handler = false;
 
 $zipArchiveLocation = "/temp/EF21.zip";
 
@@ -24,14 +28,114 @@ while(ord($csvData[0]) > 127) $csvData = substr($csvData, 1);
 
 $csvParser = new parseCSV();
 $csvParser->delimiter = ";";
-$csvContentsQueryable =  from($csvParser->parse_string($csvData));
+$csvContentsQueryable =  from($csvParser->parse_string(utf8_decode($csvData)));
 
-$existingRecordsQueryable = from($db->query("SELECT * FROM dealerentry"));
+try {
+    
+    $database->startTransaction();
 
-$oldRecords = $existingRecordsQueryable
-    ->where(function($row) use ($csvContentsQueryable) { return $csvContentsQueryable->all('$v["Reg No."] != '.$row['RegistrationNumber']); })
-    ->toArray();
+    $existingRecordsQueryable = from($database->query("SELECT * FROM dealer"));
 
-print_r($oldRecords);
+    $oldRecords = $existingRecordsQueryable
+        ->where('$v["IsDeleted"] == 0')
+        ->where(function($row) use ($csvContentsQueryable) { return $csvContentsQueryable->all('$v["Reg No."] != '.$row['RegistrationNumber']); })
+        ->toArray();
+        
+    echo sprintf("Soft deleting old records on '%s'\n", 'dealer');
+
+    from($oldRecords)->each(function($row) use($database) {
+        echo sprintf("Deleting %s\n", $row['RegistrationNumber']);
+        $database->update("dealer", array(
+                "LastChangeDateTimeUtc" => $database->sqleval("utc_timestamp()"),
+                "IsDeleted" => 1
+        ), "Id=%s", $row["Id"]);
+    });
+
+    echo sprintf("Inserting / updating records on '%s'\n", 'dealer');
+
+    from($csvContentsQueryable)->each(function($record) use($database, $existingRecordsQueryable, $zipArchiveLocation) {
+        $row = array(
+            "LastChangeDateTimeUtc" => $database->sqleval("utc_timestamp()"),
+            "IsDeleted" => "0",
+            "RegistrationNumber" => $record["Reg No."],
+            "AttendeeNickname" => $record["Nick"],
+            "DisplayName" => $record["Display Name"],
+            "ShortDescription" => $record["Short Description"],
+            "AboutTheArtistText" => $record["About the Artist"],
+            "AboutTheArtText" => $record["About the Art"],
+            "WebsiteUri" => $record["Website"],
+            "ArtPreviewCaption" => $record["Art Preview Caption"]
+        );
+        
+        $zipContentsQueryable = getZipContentsAsQueryable($zipArchiveLocation);
+        
+        // Artist Thumbnail
+        $artistThumbnailImageEntry = from($zipContentsQueryable)
+            ->where(function($v) use($row) { return (strpos($v["name"], sprintf("thumbnail_%s.", $row['RegistrationNumber'])) !== false); })
+            ->singleOrDefault();
+
+        $artistThumbnailImageKey = sprintf("dealer:artistThumbnailImage[%s]", $row['RegistrationNumber']);
+        
+        if (!$artistThumbnailImageEntry) {
+            $row["ArtistThumbnailImageId"] = null;
+            deleteImageByTitle($database, $artistThumbnailImageKey); 
+        } else {
+            $imageData = getZipContentOfFile($zipArchiveLocation, $artistThumbnailImageEntry['name']);
+            $row["ArtistThumbnailImageId"] = insertOrUpdateImageByTitle($database, $imageData, $artistThumbnailImageKey);
+        }
+        
+        // Artist Image
+        $artistImageEntry = from($zipContentsQueryable)
+            ->where(function($v) use($row) { return (strpos($v["name"], sprintf("artist_%s.", $row['RegistrationNumber'])) !== false); })
+            ->singleOrDefault();
+
+        $artistImageKey = sprintf("dealer:artistImage[%s]", $row['RegistrationNumber']);
+        
+        if (!$artistImageEntry) {
+            $row["ArtistImageId"] = null;
+            deleteImageByTitle($database, $artistImageKey); 
+        } else {
+            $imageData = getZipContentOfFile($zipArchiveLocation, $artistImageEntry['name']);
+            $row["ArtistImageId"] = insertOrUpdateImageByTitle($database, $imageData, $artistImageKey);
+        }
+        
+        // Art Preview Image
+        $artPreviewImageEntry = from($zipContentsQueryable)
+            ->where(function($v) use($row) { return (strpos($v["name"], sprintf("artist_%s.", $row['RegistrationNumber'])) !== false); })
+            ->singleOrDefault();
+
+        $artPrevieyImageKey = sprintf("dealer:artPreviewImage[%s]", $row['RegistrationNumber']);
+        
+        if (!$artPreviewImageEntry) {
+            $row["ArtPreviewImageId"] = null;
+            deleteImageByTitle($database, $artPrevieyImageKey); 
+        } else {
+            $imageData = getZipContentOfFile($zipArchiveLocation, $artPreviewImageEntry['name']);
+            $row["ArtPreviewImageId"] = insertOrUpdateImageByTitle($database, $imageData, $artPrevieyImageKey);
+        }
+
+        $existingRow = from($existingRecordsQueryable)->where('$v["RegistrationNumber"] == ' . $row['RegistrationNumber'])->singleOrDefault();
+            
+        if ($existingRow) {
+            echo sprintf("Updating %s\n", $row['RegistrationNumber']);
+            $database->update("dealer", $row, "Id=%s", $existingRow["Id"]);
+        } else {
+            $row["Id"] = $database->sqleval("uuid()");
+            echo sprintf("Inserting %s\n", $row['RegistrationNumber']);
+            $database->insert("dealer", $row);
+        }
+    });
+    
+    echo "Commiting changes to database\n";
+    $database->commit();
+    
+} catch (Exception $e) {
+    
+    var_dump($e->getMessage());
+    
+    echo "Rolling back changes to database\n";
+    $database->rollback();
+    
+}
 
 ?>
--- Server/Shared/helpers.php
+++ Server/Shared/helpers.php
@@ -43,4 +43,63 @@ function getZipContentOfFile($zipFilePath, $name) {
     return $contents;
 }
 
+function exceptionErrorHandler($severity, $message, $file, $line) {
+    if (!(error_reporting() & $severity)) {
+        return;
+    }
+    throw new ErrorException($message, 0, $severity, $file, $line);
+}
+
+function GUID()
+{
+	if (function_exists('com_create_guid') === true)
+	{
+		return trim(com_create_guid(), '{}');
+	}
+
+	return strtolower(sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535)));
+}
+
+function insertOrUpdateImageByTitle($database, $imageData, $title){
+    $imageInfo = getimagesizefromstring($imageData);
+    $existingImageRow = $database->queryFirstRow("SELECT * FROM image WHERE Title=%s", $title);
+    $imageId = $existingImageRow ? $existingImageRow["Id"] : GUID();
+    
+    $row = array(
+        "Id" => $imageId,
+        "LastChangeDateTimeUtc" => $database->sqleval("utc_timestamp()"),
+        "IsDeleted" => "0",
+        "Title" => $title,
+        "Height" => $imageInfo[1],
+        "Width" => $imageInfo[0],
+        "MimeType" => $imageInfo['mime'],
+        "FileSizeInBytes" => strlen($imageData),
+        "Url" => sprintf("{Endpoint}/ImageData/%s", $imageId)
+    );
+    
+    if (!$existingImageRow) {
+        $database->insert("imagedata", array("Id" => $row["Id"], "Data" => $imageData));
+        $database->insert("image", $row);
+    } else {
+        $existingDataRow = $database->queryFirstRow("SELECT * FROM imagedata WHERE Id=%s", $imageId);
+
+        if ($existingDataRow["Data"] != $imageData) {
+            $database->update("imagedata", array("Id" => $row["Id"], "Data" => $imageData), "Id=%s", $imageId);
+            $database->update("image", $row, "Id=%s", $imageId);
+        }
+    }
+
+    return $imageId;
+}
+
+function deleteImageByTitle($database, $title) {
+    $row = $database->queryFirstRow("SELECT * FROM image WHERE Title=%s", $title);
+    if (!$row) return;
+    
+    $database->update("image", array(
+        "LastChangeDateTimeUtc" => $database->sqleval("utc_timestamp()"),
+        "IsDeleted" => 1), "Id=%s", $row["Id"]);
+    $database->query("DELETE FROM imagedata WHERE Id=%s", $row["Id"]);        
+}
+
 ?>
